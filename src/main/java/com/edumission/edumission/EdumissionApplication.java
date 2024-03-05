8b9fdfee6cf750f37d8d876df356a3a93ddc715f
animesh2cool <animeshmannaece@gmail.com>
2024-03-05 19:25:20 +0530

    2nd commit

---
+++ src/main/java/com/edumission/edumission/EdumissionApplication.java
@@ -0,0 +1,13 @@
+package com.edumission.edumission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdumissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdumissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edumission/edumission/EdumissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumission.edumission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdumissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
